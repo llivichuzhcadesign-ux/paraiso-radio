b7679f6fd9a762de1f4359c9908d9303d564c443
llivichuzhcadesign-ux <llivichuzhca.design@gmail.com>
2026-08-19 11:06:41 -0400

    Add radio configuration

---
+++ config.js
@@ -0,0 +1,9 @@
+window.RADIO_CONFIG = {
+  stationName: "PARAÍSO",
+  tagline: "New York • Ecuador",
+  demoMode: true,
+  azuracastBaseUrl: "",
+  stationShortcode: "",
+  streamUrl: "",
+  refreshMs: 15000
+};
